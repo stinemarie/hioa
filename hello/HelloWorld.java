ae6fec292379a560525d8db1f323188290ace43a
Matias Hermanrud Fjeld <mhf@hex.no>
2013-08-16 14:48:39 +0200

    Extended HelloWorld.java with name prompt

--- hello/HelloWorld.java
+++ hello/HelloWorld.java
@@ -1,7 +1,12 @@
+import java.util.Scanner;
+
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Hello, World");
+            Scanner scanner = new Scanner (System.in);
+            System.out.print("What's your name? ");
+            String name  = scanner.next();
+            System.out.println("Hello, " + name);
     }
 
 }
