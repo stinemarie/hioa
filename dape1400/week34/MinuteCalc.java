228681ea3c9315b46d6c15a495bebc3c96db0a01
Matias Hermanrud Fjeld <mhf@hex.no>
2013-08-21 13:53:31 +0200

    Extended MinuteCalc to calcualte hours, minutes and seconds (Task 8)

--- dape1400/week34/MinuteCalc.java
+++ dape1400/week34/MinuteCalc.java
@@ -1,16 +1,27 @@
-// Week 34, Task 7
+// Week 34, Task 7 and 8
 
 import javax.swing.JOptionPane;
 
 public class MinuteCalc {
+    protected static final int SECONDS_PER_MINUTE = 60;
+    protected static final int SECONDS_PER_HOUR = SECONDS_PER_MINUTE * 60;
+
     public static void main (String[] args) {
-        int seconds, minutes;
+        int secondsInputByUser;
+        int hours, minutes, seconds;
 
         // Ask the user for number of seconds
-        seconds = Integer.decode(JOptionPane.showInputDialog("Enter a whole number of seconds."));
-        // Calculate number of whole minutes (throwing away the reminder)
-        minutes = seconds / 60;
-        // Print minutes
-        JOptionPane.showMessageDialog(null, "" + seconds + " >= " + minutes + " minutes");
+        secondsInputByUser = Integer.decode(JOptionPane.showInputDialog("Enter a whole number of seconds."));
+
+        // Calculate number of whole minutes
+        hours = secondsInputByUser / SECONDS_PER_HOUR;
+        seconds = secondsInputByUser % SECONDS_PER_HOUR;
+        // ... and hours and remaining seconds
+        minutes = seconds / SECONDS_PER_MINUTE;
+        seconds = seconds % SECONDS_PER_MINUTE;
+
+        // Print hours, minutes and seconds
+        JOptionPane.showMessageDialog(null, "" + secondsInputByUser + " seconds = " + hours + " hours, "
+                                      + minutes + " minutes, " + seconds + " seconds");
     }
 }
