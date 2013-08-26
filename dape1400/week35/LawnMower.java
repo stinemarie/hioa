f83a3972913f451cb40479259728dee1d1a56408
Matias Hermanrud Fjeld <mhf@hex.no>
2013-08-26 19:29:26 +0200

    Stubbed out the LawnMower class

---
+++ dape1400/week35/LawnMower.java
@@ -0,0 +1,39 @@
+/**
+
+Oppgave 7
+
+Skriv et program som leser inn lengden og bredden til en
+rektangulær plen, samt lengden og bredden til et rektangulært hus
+som står på plenen, alle mål i antall hele meter. (Alt utenom det
+arealet som huset opptar regner vi som plen.)
+
+Programmet skal beregne og skrive ut hvor lang tid det tar å klippe
+plenen når klippehastigheten er 0.4 kvadratmeter pr. sekund.
+
+Tiden skal skrives ut i antall timer, minutter og sekunder.
+
+Programmet skal sjekke at inndataene er konsistente, det vil si at
+huset ikke har større lengde eller bredde enn det plenen har.
+
+I tilfelle feil i inndata skal programmet skrive ut en feilmelding
+istedenfor å foreta beregning.
+
+**/
+
+public class LawnMower
+{
+    public static void main ( String[] args )
+    {
+        // Input lawn and house width and length
+
+        // If any lawn dimension is smaller than the corresponding
+        // house dimension, notify the user and stop execution.
+
+        // Calculate lawn area
+        // Calculate cutting time from cutting rate and lawn area
+
+        // Convert seconds to hours, minutes and seconds
+
+        // Display the cutting time.
+    }
+};
