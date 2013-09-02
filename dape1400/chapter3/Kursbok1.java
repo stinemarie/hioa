408ec7b1f4e0d6d027f96cb6dd5d466a4cd1ed65
Matias Hermanrud Fjeld <mhf@hex.no>
2013-09-02 10:49:39 +0200

    Typed in classes from text (Task 1)

---
+++ dape1400/chapter3/Kursbok1.java
@@ -0,0 +1,9 @@
+import javax.swing.JOptionPane;
+
+public class Kursbok1
+{
+    public void visTittel()
+    {
+        JOptionPane.showMessageDialog( null, "Kursbok" );
+    }
+}
---
+++ dape1400/chapter3/Kursboktest1.java
@@ -0,0 +1,9 @@
+public class Kursboktest1 {
+    public static void main ( String[] args ) {
+        // Oppretter et Kursbok1-objekt
+        Kursbok1 protokoll = new Kursbok1();
+
+        // Gjør all på objektets visTittel()-metode:
+        protokoll.visTittel();
+    }
+}
