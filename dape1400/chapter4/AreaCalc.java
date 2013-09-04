41fa3fd843e9b049bf37a6aa886b2ddcb135a681
Matias Hermanrud Fjeld <mhf@hex.no>
2013-09-04 16:17:50 +0200

    Extended AreaCalc with circumference and loop (Task 6)

--- dape1400/chapter4/AreaCalc.java
+++ dape1400/chapter4/AreaCalc.java
@@ -1,12 +1,37 @@
+/*
+
+I kapittel 2, oppgave 5 skulle du lage et program som leste inn radien
+til en sirkel og skrev ut sirkelens areal. Lag en ny versjon av dette
+programmet ved at programmet også beregner og skriver ut sirkelens
+omkrets. (Formelen for omkrets er 2 * Π * r.)
+
+Dessuten skal innlesing av radius og utskrift av areal og omkrets
+foretas gjentatte ganger, inntil det leses inn en negativ verdi for
+radien. (Utskrift av areal og omkrets kan gjøres i en og samme
+dialogboks.)
+
+ */
+
 import javax.swing.JOptionPane;
 import java.lang.Math;
 
 public class AreaCalc {
     public static void main (String[] args) {
-        double radius, area;
+        double radius, area, circumference;
         
-        radius = Double.parseDouble(JOptionPane.showInputDialog("Enter the radius of the circle:"));
-        area = Math.PI * Math.pow(radius, 2);
-        JOptionPane.showMessageDialog(null, "The area of the circle is " + area);
+        while ( true )
+        {
+            radius = Double.parseDouble(JOptionPane.showInputDialog("Enter the radius of the circle:"));
+
+            if ( radius < 0 )
+                break;
+
+            area = Math.PI * Math.pow(radius, 2);
+            circumference = 2 * Math.PI * radius;
+
+            JOptionPane.showMessageDialog(null,
+                                          "The area of the circle is " + area
+                                          + ",\nand the circumference is " + circumference);
+        }
     }
 }
