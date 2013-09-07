8d44271662d1533a00d990e65980cafff69fa308
Matias Hermanrud Fjeld <mhf@hex.no>
2013-09-07 16:30:44 +0200

    Added task description (task 2)

--- dape1400/oblig1/OddSum.java
+++ dape1400/oblig1/OddSum.java
@@ -1,3 +1,25 @@
+/*
+
+Oppgave 2
+---------
+
+Lag et applikasjonsprogram som beregner og skriver ut summen av alle
+oddetall mellom to grenser som brukeren skriver inn.
+
+Programmet skal starte med å lese inn nedre og øvre grense for
+summen. Dersom innlest øvre grense er mindre enn eller lik nedre
+grense, skal programmet skrive ut en melding til brukeren om dette og
+foreta ny innlesing. Dette skal gjentas inntil det er lest inn en øvre
+grense som er større enn nedre grense.
+
+Når godkjente grenser er lest inn, skal programmet beregne nevnte sum
+og summen skal så skrives ut.
+
+Grensene trenger ikke å være partall, men hvis en eller begge er det,
+skal de tas med i summen.
+
+ */
+
 public class OddSum
 {
     public static int sum(int m, int n)
