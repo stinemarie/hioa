2f33a4fd920fc669106a453a95091159e3c0b217
Matias Hermanrud Fjeld <mhf@hex.no>
2013-09-08 10:17:54 +0200

    Added templates from teacher (task 3)

---
+++ dape1400/oblig1/Sirkel/Sirkel.java
@@ -0,0 +1,22 @@
+<  nødvendig(e) import-setning(er)  >
+
+public class Sirkel
+{
+    <  Datafelt for sirkelens radius. >
+
+    <  Konstruktør som skal gi startverdi til sirkelens radius.
+        NB! Hvis startverdien er negativ, skal radius settes til 0. >
+
+    <  set-metode for sirkelens radius. >
+
+    <  get-metode for sirkelens radius.  >
+
+    <  Metode som beregner og returnerer sirkelens omkrets.  >
+
+    <  Metode som beregner og returnerer sirkelens areal.  >
+
+    <  Metode som viser sirkelens radius, diameter, omkrets
+       og areal i et dialogvindu. NB! Du SKAL bruke klassens
+       metoder for å beregne disse verdiene. >
+
+} // end of class Sirkel
---
+++ dape1400/oblig1/Sirkel/SirkelTest.java
@@ -0,0 +1,17 @@
+<  nødvendig(e) import-setning(er)  >
+
+public class SirkelTest
+{
+    public static void main(String[] args)
+    {
+    <  Les inn sirkelens radius fra brukeren. >
+
+    <  Så lenge radius er positiv:
+       Opprett et Sirkel-objekt, som ved opprettelsen blir tilført
+       den radius som brukeren har skrevet inn.
+       Skriv sirkelens radius, diameter, omkrets og areal ut på skjermen
+       ved å bruke en eller flere metoder i Sirkel-klassen.
+       Les inn sirkelens radius på nytt fra brukeren. >
+
+    }
+} // end of class SirkelTest
