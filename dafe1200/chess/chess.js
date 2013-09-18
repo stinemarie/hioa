66ba1debcce8555b08abdcbd91ef1bb99cf39b5a
Matias Hermanrud Fjeld <mhf@hex.no>
2013-09-18 23:26:39 +0200

    Highlight chess cell when moving pieces

--- dafe1200/chess/chess.js
+++ dafe1200/chess/chess.js
@@ -1,6 +1,19 @@
 var Chess = {
- init : function () {
+ init : function() {
     $(".chessboard td .piece").draggable();
+    $(".chessboard td").droppable({ over: Chess.over, out: Chess.out, drop: Chess.drop });
+  },
+
+ over: function( event, ui ) {
+    $(event.target).addClass('over');
+  },
+
+ out: function( event, ui ) {
+    $(event.target).removeClass('over');
+  },
+
+ drop: function( event, ui ) {
+    $(event.target).removeClass('over');
   }
 };
 
