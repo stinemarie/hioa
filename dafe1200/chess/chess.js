4abd5fe512567194ea042b2b399f03733c027303
Matias Hermanrud Fjeld <mhf@hex.no>
2013-09-19 01:27:03 +0200

    Use different discard piles for black and white pieces

--- dafe1200/chess/chess.js
+++ dafe1200/chess/chess.js
@@ -1,4 +1,9 @@
 var Chess = {
+ colors: {
+  white: ["\u2654", "\u2655", "\u2656", "\u2657", "\u2658", "\u2659"],
+  black: ["\u265A", "\u265B", "\u265C", "\u265D", "\u265E", "\u265F"],
+  },
+
  init : function() {
     $(".chessboard td .piece").draggable();
     $(".chessboard td").droppable({ over: Chess.over, out: Chess.out, drop: Chess.drop });
@@ -14,10 +19,29 @@ var Chess = {
 
  drop: function( event, ui ) {
     $(event.target).removeClass('over');
-    $('.discarded').append($(event.target).children().detach().draggable("destroy"));
-    $(event.target).append(ui.draggable.detach());
+
+    // Remove old piece
+    $(event.target).children().each(function(i) {
+        Chess.discard($(this));
+      });
+
+    $(event.target).append(ui.draggable.detach()); // Insert new piece
     ui.draggable.css("top", "0px");
     ui.draggable.css("left", "0px");
+  },
+
+ discard: function( piece ) {
+    piece.draggable("destroy"); // Discarded pieces can't be moved
+    piece.detach(); // Remove from board
+    $('.discarded.' + Chess.color(piece)).append(piece); // Add to discarded pile
+  },
+
+ color: function( piece ) {
+    piece = piece.text();
+    if (jQuery.inArray(piece, Chess.colors.white) >= 0)
+      return "white";
+    else if (jQuery.inArray(piece, Chess.colors.black) >= 0)
+      return "black";
   }
 };
 
